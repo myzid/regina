a2beafd4d0aa7d7ce4e7668cf81fc8f1ed03790b
myzid <143302655+myzid@users.noreply.github.com>
2023-10-23 22:45:30 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 clear
 fun_bar() {
     CMD[0]="$1"
@@ -29,26 +26,20 @@ fun_bar() {
     echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
     tput cnorm
 }
-res1() {
+update() {
     wget https://raw.githubusercontent.com/myzid/regina/main/menu.zip
-    unzip menu.zip
-    chmod +x menu/*
-    mv menu/* /usr/local/sbin
-    rm -rf menu
-    rm -rf menu.zip
-    rm -rf update.sh
+    unzip menu.zip ; chmod +x menu/* ; mv menu/* /usr/local/sbin
+    rm -rf menu ; rm -rf menu.zip ; rm -rf update.sh
 }
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT       \e[0m"
+echo -e " \e[1;97;101m               UPDATE SCRIPT            \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 echo -e "  \033[1;91m Update Script Service\033[1;37m"
-fun_bar 'res1'
+fun_bar 'update'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
 menu
-
-###########- COLOR CODE -##############
